e6455121d50d381c32023f249f92fb32b0af39a9
brisberg <brandon.risberg@gmail.com>
2014-08-09 23:28:03 +0000

    Changed to read from index.html

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+var fs = require('filesys')
 var express = require('express')
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+var buf = new Buffer();
+buf = fs.readFileSync("index.html");
+
 app.get('/', function(request, response) {
-  response.send('Hello World! 2')
+  response.send(buf.toString('utf-8'));
 })
 
 app.listen(app.get('port'), function() {
